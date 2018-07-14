a78687dbca7d4244a24c21c6bd5ed8daa3e39f60
hansel-no-kioku <hansel-no-kioku@users.noreply.github.com>
2018-07-14 22:17:40 +0900

    Add hello world sample

---
+++ samples/01-hello-world/src/Main01.purs
@@ -0,0 +1,20 @@
+-- Hello World
+
+module Main01 where
+
+import Prelude
+
+import Effect (Effect)
+import Phina as P
+
+main :: Effect Unit
+main = do
+  let scenes = P.MainScene setuptScene
+  game ← P.newGame {title: "Hello World"} scenes
+  P.runGame game
+
+setuptScene ∷ P.SetupScene P.DisplayScene () (score ∷ Int)
+setuptScene _ exit scene = do
+  pos ← P.getCenterPos scene
+  label ← P.newLabel {x: pos.x, y: pos.y, text: "Hello World!"}
+  P.addChild label scene
