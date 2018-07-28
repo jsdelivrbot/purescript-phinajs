7d2e367acccd17ffe5aeae340f05aab986dca67a
hansel-no-kioku <hansel-no-kioku@users.noreply.github.com>
2018-07-28 11:00:46 +0900

    Fix typo

--- samples/01-hello-world/src/Main01.purs
+++ samples/01-hello-world/src/Main01.purs
@@ -9,12 +9,12 @@ import Phina as P
 
 main :: Effect Unit
 main = do
-  let scenes = P.SceneListDefault P.Main setuptScene
+  let scenes = P.SceneListDefault P.Main setupScene
   game ← P.newGame {title: "Hello World"} scenes
   P.runGame game
 
-setuptScene ∷ P.SetupScene P.DisplayScene () (score ∷ String)
-setuptScene _ exit scene = do
+setupScene ∷ P.SetupScene P.DisplayScene () (score ∷ String)
+setupScene _ exit scene = do
   pos ← P.getCenterPos scene
   label ← P.newLabel {x: pos.x, y: pos.y, text: "Hello World!"}
   P.addChild label scene
